9788d161bdc02933c63be460edbb004214043eac
Uijin Lee <100072451+dml113@users.noreply.github.com>
2025-06-08 00:27:17 +0900

    Update install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -5,6 +5,10 @@ sudo mv ./kubectl /usr/bin/
 sudo ln -s /usr/bin/kubectl /usr/local/bin/k
 k version --client
 
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/bin/
+eksctl version
+
 yum install -y docker
 usermod -a -G docker ec2-user
 systemctl enable --now docker
