bc66c84573dceebca6b74f904204a15afad93ee3
Uijin Lee <100072451+dml113@users.noreply.github.com>
2025-06-08 00:01:16 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ chmod +x ./kubectl
 sudo mv ./kubectl /usr/bin/
 sudo ln -s /usr/bin/kubectl /usr/local/bin/k
 k version --client
+
 yum install -y docker
 usermod -a -G docker ec2-user
 systemctl enable --now docker
+
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
