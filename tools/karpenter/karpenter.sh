ba69aa0cf6f1a46dd8cb654956bf534564fc25a3
Uijin Lee <100072451+dml113@users.noreply.github.com>
2025-06-08 00:23:10 +0900

    Update karpenter.sh

--- tools/karpenter/karpenter.sh
+++ tools/karpenter/karpenter.sh
@@ -1,12 +1,3 @@
-export KARPENTER_NAMESPACE="kube-system"
-export KARPENTER_VERSION="1.5.0"
-export K8S_VERSION="1.32"
-export AWS_PARTITION="aws"
-export CLUSTER_NAME="<CLUSTER_NAME>"
-export AWS_DEFAULT_REGION="ap-northeast-2"
-export AWS_ACCOUNT_ID="$(aws sts get-caller-identity --query Account --output text)"
-export TEMPOUT="$(mktemp)"
-
 curl -fsSL https://raw.githubusercontent.com/aws/karpenter-provider-aws/v"${KARPENTER_VERSION}"/website/content/en/preview/getting-started/getting-started-with-karpenter/cloudformation.yaml  > "${TEMPOUT}" \
 && aws cloudformation deploy \
   --stack-name "Karpenter-${CLUSTER_NAME}" \
